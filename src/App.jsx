f81a1ecb2b3999ed224a799c67fcb86f56f3afde
kabuzo13 <beahonigmann@gmail.com>
2024-03-13 20:30:17 +0000

    installed chart packages and set up a basic chart

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import CategoryChart from './components/category-chart/categoryChart'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <CategoryChart/>
+      </div>
+    </>
+  )
+}
+
+export default App
