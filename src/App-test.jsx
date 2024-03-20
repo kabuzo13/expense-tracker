6e54b9a8865dd4d3c49658a35b91b7a2d83d1059
kabuzo13 <beahonigmann@gmail.com>
2024-03-20 19:34:55 +0000

    added expense gauge tracker html and converted to jsx

--- src/App-test.jsx
+++ src/App-test.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
 import CategoryChart from './components/category-chart/categoryChart'
+import Balance from './components/balance indicator/balance'
 
 function App() {
 
   return (
     <>
       <div>
+      <Balance/>
       <CategoryChart/>
       </div>
     </>
